87e0e79cf6f3aedd3f113ab0ddbb189aa4cf48ec
Benjamin Chamness <bjchamness@yahoo.com>
2021-03-01 17:16:42 -0500

    comment class

---
+++ 24hr.Data/Comments.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24hr.Data
+{
+    public class Comments
+    {
+        [Key]
+        public int CommentId { get; set; }
+        
+        [MaxLength(500, ErrorMessage = "You have too much to say; Please limit to 500 characters.")]
+        public string Text { get; set; }
+        
+        public Guid Author { get; set; }
+
+        public virtual List<string> Replies { get; set; } = new List<string>();
+
+        [Required]
+        [ForeignKey(nameof(Post))]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+
+
+    }
+}
+
+    //(virtual list of Replies)
+   //(Foreign Key to Post via Id w/ virtual Post)
--- 24hr.Data/IdentityModels.cs
+++ 24hr.Data/IdentityModels.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+//using Microsoft.AspNetCore.Identity;
+
 
 namespace _24hr.Data
 {
@@ -62,5 +64,4 @@ public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUse
     {
         HasKey(iur => iur.UserId);
     }
-}
 }
